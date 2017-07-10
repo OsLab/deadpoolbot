5133cf277a3209c8d88e7d1993ed235538a4699d
Michael COULLERET <michael@coulleret.pro>
2017-07-10 15:05:59 +0200

    Add Token security

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new OsLab\SecurityApiBundle\OsLabSecurityApiBundle(),
             new Zeichen32\GitLabApiBundle\Zeichen32GitLabApiBundle(),
             new AppBundle\AppBundle(),
         ];
